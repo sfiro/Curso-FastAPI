e89df7e4ab1662e275e1e0b31992a34df2c41b6c
Debbie Arredondo <chevi6@gmail.com>
2023-05-16 22:25:37 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+#python
+from typing import Optional
 
+#pydantic
+from pydantic import BaseModel
+
+#fastAPI
+from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#models
+
+class Person(BaseModel):
+    first_name : str
+    last_name : str
+    age : int
+    hair_color : Optional[str] = None
+    is_married : Optional[bool] = None
+
+
 
 @app.get("/")
 def home():
     return {"hello":"world"}
+
+# request and response body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
+
