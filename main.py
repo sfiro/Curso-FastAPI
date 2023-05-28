77abc2f6877282d5deaf25690ed5a567b35714be
Debbie Arredondo <chevi6@gmail.com>
2023-05-28 00:31:21 -0500

    cookies_and_headers

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from pydantic import PaymentCardNumber
 #fastAPI
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 
 app = FastAPI()
 
@@ -177,6 +177,8 @@ def update_person(
     results.update(location.dict())
     return results
 
+# forms 
+
 @app.post(
     path = "/login",
     response_model = LoginOut,
@@ -187,3 +189,29 @@ def login(
     password: str = Form(...)
     ):
     return LoginOut(username=username)
+
+# Cookies and headers parameters
+@app.post(
+    path="/contact",
+    status_code=status.HTTP_200_OK
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_length=20
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+):
+    return user_agent
